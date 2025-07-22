e71e53967b136e7e0e8343acc48101d44e3f7029
Viresh Tarapur <tarapurviresh@gmail.com>
2025-07-22 17:05:15 +0530

    Loop.py

--- python/Loop.py
+++ python/Loop.py
@@ -21,3 +21,107 @@
 x =("viresh","sudeepa","apple","pratik")
 for x in("viresh"):
     print(x)
+
+
+n = int(input("enter the a number :"))
+
+for i in range(2, n):
+    if(n%i)==0:
+        print("number is not prime")
+        break
+else:
+    print("the number is prime")
+
+
+
+
+
+
+for name in l:
+    if(name.startswith("v")):
+        print(f"hello {name}")
+
+
+n = int(input("enter the number : "))
+for i in range(1,11):
+    print(f"{i*n}") 
+
+
+
+i =1
+for i in range(10):
+    if(i==3):
+        break
+    print(i)
+
+
+i=1
+for i in range(7):
+    if(i==6):
+        continue
+    print(i)
+
+
+
+
+n = int(input("enter the a number :"))
+
+for i in range(2, n):
+    if(n%i)==0:
+        print("number is not prime")
+        break
+else:
+    print("the number is prime")
+
+
+
+
+l = ["viresh","vigenesh","rahul","hero"]
+
+
+
+name = [2,4,6,7,7,8,9,9,"guru","viresh", "hello", "sudeep"]
+i = 0
+
+while i < len(name):
+    if name[i] == "viresh":
+        break
+    print(name[i])
+    i += 1
+
+
+
+viresh = [1, "viresh", "giresh", "harish", "hello"]
+i = 0
+while i < len(viresh):
+    print(viresh[i])
+    i += 1
+
+
+
+i = 1
+v =int(input("enter the size of your name ")) 
+while (i<=v):
+    x=input("enter the name letters:")
+    print(x)
+    i +=1
+    break
+
+
+i=0
+while(i<5):
+    print("viresh")
+    i +=1
+
+
+i = 0
+v =int(input("enter the size of your name ")) 
+while (i<=v):
+    print(input("enter the name letters:"))
+    i +=1
+
+
+
+
+
+
